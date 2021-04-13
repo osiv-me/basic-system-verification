005da29e4851f493077f29ce9dd8e292e9af7271
Erik Erlandsson <Erik.Erlandsson@cybercom.com>
2021-04-13 17:42:59 +0200

    assignment 2 work day 1

--- assignment 2/fixtures.py
+++ assignment 2/fixtures.py
@@ -2,5 +2,5 @@ import pytest
 import shutil
 
 @pytest.fixture()
-def db_setup():
+def db_clean():
 	shutil.copy('/home/pft/restapi/point-of-sale/pos_bak.db', '/home/pft/restapi/point-of-sale/pos.db')

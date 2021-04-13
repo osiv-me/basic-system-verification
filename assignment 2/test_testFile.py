2b987dd0fe7f2fff52f6dc5e388a3c31f5b7463e
Erik Erlandsson <Erik.Erlandsson@cybercom.com>
2021-04-13 15:50:44 +0200

    pushing up rest api doc

--- assignment 2/test_testFile.py
+++ assignment 2/test_testFile.py
@@ -4,5 +4,8 @@ from fixtures import db_setup
 class TestClass():
 	def test_get_customers(self,db_setup):
 		response=requests.get('http://localhost:6399/customers')
-		customers=response.json()
+		customers=response.json()		
 		assert len(customers)==1
+		assert response.status_code == 200
+
+	
